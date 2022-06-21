41c8171ba6c2f889eb6bf8e1c43a280d4aa0828a
bewlief <71935665@qq.com>
2022-06-21 08:02:49 +0800

    add one simple java+spring applicaiton for testing

---
+++ a-java-app/src/main/java/com/xhoe/sretestfoo/SreTestFooApplication.java
@@ -0,0 +1,13 @@
+package com.xhoe.sretestfoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreTestFooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SreTestFooApplication.class, args);
+    }
+
+}
